f5f4f3d62ab13653a076b5e4dc82f5db9d93b4d2
ryoryo714 <ilovesoccerhr94@gmail.com>
2020-11-22 08:36:54 +0900

    バリデーションコード整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name_kana, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :birth_day, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :family_name
+      validates :first_name
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+  end
+
+  
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/
   validates :password, presence: true, length: { minimum: 6},format: { with: VALID_PASSWORD_REGEX}
