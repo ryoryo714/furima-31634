a58cf9eb9883bb3f7e62f2249f8a4fc6a3b84f5e
ryoryo714 <ilovesoccerhr94@gmail.com>
2020-12-02 17:12:19 +0900

    商品削除機能修正4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,9 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path unless current_user == @item.user
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user.id
+      @item.destroy
+     end
+     redirect_to root_path
   end
 
   private
